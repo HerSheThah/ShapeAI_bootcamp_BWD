e8af186156058e004ce6cbecde9e81a15ca13236
HerSheThah <harshisubb510@gmail.com>
2021-06-26 20:46:07 +0530

    Add files via upload

---
+++ src/components/Info.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+    <div class="note">
+      <h1>Javasccript Bootcamp</h1>
+      <p>This is a javascript bootcamp for beginers.</p>
+    </div>
+    <div class="note">
+    <h1>Javasccript Bootcamp</h1>
+    <p>This is a javascript bootcamp for beginers.</p>
+  </div>
+  <div class="note">
+  <h1>Javasccript Bootcamp</h1>
+  <p>This is a javascript bootcamp for beginers.</p>
+</div>
+</div>
+  );
+}
+export default Info;
